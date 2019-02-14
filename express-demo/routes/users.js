b0a84411ec807c71b0fd826a49cbf42fd703c82e
Alex <alexnou@gmail.com>
2019-02-14 11:00:25 -0700

    Added /me

--- express-demo/routes/users.js
+++ express-demo/routes/users.js
@@ -1,9 +1,16 @@
+const auth = require('../middleware/auth') // authorization
 const _ = require('lodash');
 const {User, validate} = require('../models/user')
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
  
+router.get('/me', auth, async (req, res) => {
+    // Data is retrieved from the json web token
+    const user = await User.findById(req.user._id).select('-password'); // Exclude the password property
+    res.send(user);
+})
+
 router.post('/', async (req, res) => {
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
