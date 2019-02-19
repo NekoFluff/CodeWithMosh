ac43d0071c9a8aeff9103b7292ef0de1b010c7de
Alex <alexnou@gmail.com>
2019-02-19 10:02:25 -0700

    Added validateObjectId middleware

--- express-demo/routes/genres.js
+++ express-demo/routes/genres.js
@@ -3,6 +3,7 @@ const admin = require('../middleware/admin');
 const {Genre, validate} = require('../models/genre');
 const express = require('express');
 const router = express.Router();
+const validateObjectId = require('../middleware/validateObjectId')
 
 router.get('/', async (req, res) => {
   const genres = await Genre.find().sort('name');
@@ -19,7 +20,7 @@ router.post('/', auth, async (req, res) => {
   res.send(genre);
 });
 
-router.put('/:id', async (req, res) => {
+router.put('/:id', validateObjectId, async (req, res) => {
   const { error } = validate(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
 
@@ -32,7 +33,7 @@ router.put('/:id', async (req, res) => {
   res.send(genre);
 });
 
-router.delete('/:id', [auth, admin], async (req, res) => {
+router.delete('/:id', [validateObjectId, auth, admin], async (req, res) => {
   const genre = await Genre.findByIdAndRemove(req.params.id);
 
   if (!genre) return res.status(404).send('The genre with the given ID was not found.');
@@ -40,7 +41,7 @@ router.delete('/:id', [auth, admin], async (req, res) => {
   res.send(genre);
 });
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', validateObjectId, async (req, res) => {
   const genre = await Genre.findById(req.params.id);
 
   if (!genre) return res.status(404).send('The genre with the given ID was not found.');
--- express-demo/tests/integration/genres.test.js
+++ express-demo/tests/integration/genres.test.js
@@ -31,12 +31,17 @@ describe('/api/genres', () => {
 
     describe('GET/:id', () => {
         it('should return a genre if valid id is passed', async () => {
-            const genre = new Genre({ name: 'Genra A '});
+            const genre = new Genre({ name: 'Genre A '});
             await genre.save();
 
             const res = await request(server).get('/api/genres/' + genre._id);
             expect(res.status).toBe(200);
             expect(res.body).toHaveProperty('name', genre.name);
+        });
+
+        it('should return 404 if an invalid id is passed', async () => {
+            const res = await request(server).get('/api/genres/1');
+            expect(res.status).toBe(404);
         });
     });
 });
