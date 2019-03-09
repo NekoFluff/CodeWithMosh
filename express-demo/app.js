b2891bd2a386f05f279c277ff746b8ba0cc58de0
CrazyFluff <alexnou@gmail.com>
2019-03-09 16:21:56 -0700

    Added auth disable ability

--- express-demo/app.js
+++ express-demo/app.js
@@ -1,20 +1,21 @@
-const winston = require('winston');
-const express = require('express');
+const winston = require("winston");
+const express = require("express");
 const app = express();
 
-app.set('view engine', 'pug')
-app.set('views', './views') // Views for template engine
+app.set("view engine", "pug");
+app.set("views", "./views"); // Views for template engine
 
-require('./startup/logging')(app);
-require('./startup/routes')(app);
-require('./startup/database')();
-require('./startup/config')();
-require('./startup/validation')();
-require('./startup/prod')(app);
+require("./startup/cors")(app);
+require("./startup/logging")(app);
+require("./startup/routes")(app);
+require("./startup/database")();
+require("./startup/config")();
+require("./startup/validation")();
+require("./startup/prod")(app);
 
-port = process.env.PORT || 3000;
+port = process.env.PORT || 3900;
 const server = app.listen(port, () => {
-    winston.info(`Listening to port ${port}`);
+  winston.info(`Listening to port ${port}`);
 });
 
 module.exports = server;
--- express-demo/middleware/admin.js
+++ express-demo/middleware/admin.js
@@ -1,8 +1,12 @@
-module.exports = function (req, res, next) {
-    // Executed after authorization function
-    if (req.user.isAdmin) return res.status(403).send('Access Denied.')
-    // 401 = Unauthorized (When the user tries to access a protected resource but they don't provide a valid jwt)
-    // 403 = Don't try again. You can't access that resource with your current jwt
+const config = require("config");
 
-    next();
-}
+module.exports = function(req, res, next) {
+  if (!config.get("requiresAuth")) return next();
+
+  // Executed after authorization function
+  if (!req.user.isAdmin) return res.status(403).send("Access Denied.");
+  // 401 = Unauthorized (When the user tries to access a protected resource but they don't provide a valid jwt)
+  // 403 = Don't try again. You can't access that resource with your current jwt
+
+  next();
+};
